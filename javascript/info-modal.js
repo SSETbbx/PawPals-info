5a22edddf9bf2f9f3d70719af0289f962e1ce41d
G.K <152708210+SSETbbx@users.noreply.github.com>
2024-05-13 16:06:52 +0700

    all files

---
+++ javascript/info-modal.js
@@ -0,0 +1,30 @@
+const devInfo = {
+    Phuong: {
+        name: "Nguyen Minh Phuong",
+        dob: "02/11/2004",
+        institution: "RMIT University Vietnam - Hanoi Campus",
+        studentID: "s4063236",
+        email: "s4063236@rmit.edu.vn"
+    },
+    Muck: {
+        name: "Dao Thao Phuong",
+        dob: "18/09/2006",
+        institution: "RMIT University Vietnam - Hanoi Campus",
+        studentID: "s4029319",
+        email: "s4029319@rmit.edu.vn"
+    },
+    Nghia: {
+        name: "Pham Trung Nghia",
+        dob: "28/05/2005",
+        institution: "RMIT University Vietnam - Hanoi Campus",
+        studentID: "s4028630",
+        email: "s4028630@rmit.edu.vn"
+    },
+    GK: {
+        name: "Nguyen Hoang Gia Khanh",
+        dob: "30/06/2005",
+        institution: "RMIT University Vietnam - Hanoi Campus",
+        studentID: "s4035894",
+        email: "s4035894@rmit.edu.vn"
+    }
+}
